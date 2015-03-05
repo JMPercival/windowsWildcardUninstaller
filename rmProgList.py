b5e2ec0fc1fdd1e459c598b7c0abcef73fe08765
jam109 <jperci5@gmail.com>
2015-03-04 20:22:47 -0600

    Second revision
    Quite a dirty way of doing this. This script was made by me a couple years ago and I will need to clean this up quite a bit now that I know better. 
    
    This extension added 64bit capability along with shotgunning nasty .exe's to try to get rid of them.

--- rmProgList.py
+++ rmProgList.py
@@ -1,4 +1,3 @@
-from subprocess import call
 from _winreg import *
 from sys import argv
 from time import sleep
@@ -7,9 +6,10 @@ import os
 
 #global vars
 uninstallList=[]
+uninstallList2=[]
 
 def findString(app):
-  """find all keys with a certain wildcard name"""
+	"""find all keys with a certain wildcard name"""
 	found=False
 	log.write("[+]Looking for app: "+repr(app)+"\n")
 	reg=ConnectRegistry(None,HKEY_LOCAL_MACHINE)
@@ -39,7 +39,6 @@ def findString(app):
 				if uninstall != None:
 					log.write("[+]Found string and returning! String="+repr(uninstall)+"\n")
 					found=True
-				CloseKey(reg)
 				uninstallList.append(uninstall[0])
 		except EnvironmentError:
 			pass
@@ -75,7 +74,6 @@ def findString(app):
 				if uninstall != None:
 					log.write("[+]Found string and returning! String="+repr(uninstall)+"\n")
 					found=True
-				CloseKey(reg)
 				uninstallList.append(uninstall[0])
 		except EnvironmentError:
 			pass
@@ -83,6 +81,83 @@ def findString(app):
 	#close key to end
 	if found==False:
 		log.write("[+]Could not find the key for:"+app+"\n")	
+	CloseKey(reg)
+	
+	
+def findStringQuiet(app): #not using now
+	"""find all keys with a certain wildcard name"""
+	log.write("[+]Moving in to find quiet strings.")
+	found=False
+	log.write("[+]Looking for app: "+repr(app)+"\n")
+	reg=ConnectRegistry(None,HKEY_LOCAL_MACHINE)
+	key=OpenKey(reg,r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
+	uninstall=None
+	#recurse through to find all keys"""
+	keyTemp=None
+	for x in range(1024):
+		try:
+			subKeyName=EnumKey(key,x)
+		except EnvironmentError:
+			pass
+			
+		#break out of loop if the key names are the same repeated 
+		if keyTemp==subKeyName:
+			log.write("[+]Key name repeated, closing 32bit loop...\n")
+			break
+		keyTemp=subKeyName
+		
+		subKey=OpenKey(key,subKeyName)
+		#simply query the keys to find appropriate and append to uninstallList
+		try:
+			disName=QueryValueEx(subKey,"DisplayName")
+			if str(app).lower() in disName[0].lower():
+				log.write("[+]Found application name!\n")
+				uninstall=QueryValueEx(subKey,"QuietUninstallString")
+				if uninstall != None:
+					log.write("[+]Found quiet string and returning! String="+repr(uninstall)+"\n")
+					found=True
+				uninstallList2.append(uninstall[0])
+		except EnvironmentError:
+			pass
+			
+			
+	try:
+		key64=OpenKey(reg,r"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall")
+	except WindowsError:
+		log.write("[+]This is not a 64bit machine...\n")
+		CloseKey(reg)
+		return None
+		
+	keyTemp=None
+	for x in range(1024):
+		try:
+			subKeyName=EnumKey(key64,x)
+		except EnvironmentError:
+			pass
+			
+	#break out of loop if the key names are the same repeated 
+		if keyTemp==subKeyName:
+			log.write("[+]Key name repeated, closing 64bit loop...\n")
+			break
+		keyTemp=subKeyName
+		
+		subKey=OpenKey(key64,subKeyName)
+		#simply query the keys to find appropriate and append to uninstallList
+		try:
+			disName=QueryValueEx(subKey,"DisplayName")
+			if str(app).lower() in disName[0].lower():
+				log.write("[+]Found application name!\n")
+				uninstall=QueryValueEx(subKey,"QuietUninstallString")
+				if uninstall != None:
+					log.write("[+]Found quiet string and returning! String="+repr(uninstall)+"\n")
+					found=True
+				uninstallList2.append(uninstall[0])
+		except EnvironmentError:
+			pass
+			
+	#close key to end
+	if found==False:
+		log.write("[+]Could not find a quiet key for:"+app+"\n")	
 	CloseKey(reg)
 	
 def findRe(st):
@@ -110,7 +185,34 @@ def checkString(uninstall):
 			st=findRe(uninstall)
 			if st!=None:
 				refUninstall=uninstall.replace(st,"")
-				end='"'+st+'"+"'+refUninstall+' /s /v/qn"'
+				end="r'"+'"'+st+'" '+refUninstall+" /S /quiet /q /s /silent /qn /v/qn'"
+				log.write("[!!!+!!!] shotgunned the uninstall string for: "+end)
+				return end
+			else:
+				log.write("Regex broke "+uninstall+"\n")
+				return None
+			
+				
+def quietCheckString(uninstall): #not using now
+	if uninstall == None:
+		log.write("[+]Uninstall String does not hold anything... Forcing termination\n")
+		return 'quit'
+	else:
+		if 'MsiExec.exe' in uninstall:
+			striped=uninstall.split('{')
+			end=None
+			for x in striped:
+				if '}' in x:
+					end=x
+			msiexec='"MsiExec /x{'
+			end=msiexec+end
+			end+=' /quiet"'
+			return end
+		else:
+			st=findRe(uninstall)
+			if st!=None:
+				refUninstall=uninstall.replace(st,"")
+				end="r'"+'"'+st+'"'+"'"+'+"'+refUninstall+'"'
 				return end
 			else:
 				log.write("Regex broke "+uninstall+"\n")
@@ -129,7 +231,7 @@ def sortRemove(lis):
 	
 def runUninstall(func):
 	log.write('running: os.system('+func+')\n')
-	print "running: os.system("+func+")"    ###Used for testing###
+	#print "running: os.system("+func+")"    ###Used for testing###
 	os.system(eval(func))
 	sleep(120)
 	
@@ -144,6 +246,16 @@ def removeQuotes(lis):
 	end=lis.replace('"','')
 	return end
 	
+def removeDups(lis): ##taken out, not worth removing extra commands, will break the entire rest of the code...
+	end=[]
+	lis.sort()
+	temp=lis[0]
+	for x in lis[1:]:
+		if findRe(x)==findRe(temp) and '/S' in x:
+			continue
+		else: end.append(x)
+		temp=x
+	return end
 	
 def main(arg):
 	for x in arg:
@@ -152,34 +264,21 @@ def main(arg):
 	refUninstallLis=map(removeQuotes, refUninstallLis)
 	refUninstallLis=map(checkString,refUninstallLis)
 	endUninstallLis=noneFilter(refUninstallLis)
+	
+	#tried to mine registry key "QuietUninstallString" but I am going to shotgun the options anyway so I am done with this
+	#for x in arg:
+	#	findStringQuiet(x)
+	#refUninstallLis=sortRemove(uninstallList2)
+	#refUninstallLis=map(removeQuotes, refUninstallLis)
+	#refUninstallLis=map(quietCheckString,refUninstallLis)
+	#endUninstallLis+=noneFilter(refUninstallLis)
+	
 	for x in endUninstallLis:
 		runUninstall(x)
 	log.close()
-		
-"""if uninstall != None:
-	refUninstall=checkString(uninstall)
-	log.write("[+]Uninstall String:"+refUninstall+"\n")
-	#call(refUninstall, shell=True)
-	log.write("[+]Starting Uninstall and Sleeping 180 Seconds\n")
-	#sleep(180)
-log.close()
-#print "'"+refUninstall+"'"
-#refUninstall=['dir','/S','/Q']
-#call(refUninstall, shell=True)"""
-
 
 
 if __name__=="__main__":
-	log=open("remove.log",'w')
-	main(argv[1:])
-
-
-
-
-
-
-
-
-
-
-
+	dir=argv[1]
+	log=open(dir,'w+')
+	main(argv[2:])
